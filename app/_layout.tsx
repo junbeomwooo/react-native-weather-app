e95174a1f12bc0a13d6fa778edfa4c70a573a7cb
junbeomwooo <junbeom2.woo@gmail.com>
2025-06-23 21:08:19 +0200

    Studying:RN+Expo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,11 @@
 import "@/global.css";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>공통헤더이다</Text>
-      </SafeAreaView>
       <Stack />
       <StatusBar style="auto" />
     </SafeAreaProvider>
--- app/index.tsx
+++ app/index.tsx
@@ -1,79 +1,69 @@
-import { Image } from "expo-image";
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import {
-  Alert,
-  Button,
-  Platform,
-  Pressable,
-  ScrollView,
-  Switch,
-  Text,
-  TextInput,
-  View
-} from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView, Text, View } from "react-native";
 
 export default function Index() {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
-
   return (
-    <ScrollView
-      style={{ paddingTop: 200 }}
-      contentContainerStyle={{
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View>
-        <Text className="text-2xl">Hello</Text>
-        <TextInput style={{ width: 200, height: 30, borderWidth: 1 }} />
-        <Image
-          source={require("@/assets/images/adaptive-icon.png")}
-          className="w-[100px] h-[100px]"
-        />
-        <StatusBar />
-
-        <View className="mt-4">
-          <Button
-            color="#f194ff"
-            title="Press me"
-            onPress={() => {
-              if (Platform.OS === "web") {
-                window.alert("Hello, there");
-              } else {
-                Alert.alert("Hello, there");
-              }
-            }}
-          />
-        </View>
-
-        <Pressable
-          className="bg-blue-500 py-2 px-4 text-center mt-4"
-          accessibilityLabel="Hello"
-          onPress={() => {
-            if (Platform.OS === "web") {
-              window.alert("Hello, there");
-            } else {
-              Alert.alert("Hello, there");
-            }
-          }}
-        >
-          <Text className="text-center text-white font-medium">Hello</Text>
-        </Pressable>
+    <View className="flex-1">
+      <View className="flex-1 bg-orange-500 justify-center items-center">
+        <Text style={{ fontSize: 45 }} className="font-semibold">
+          Copenhagen
+        </Text>
       </View>
-
-       <SafeAreaProvider>
-      <SafeAreaView className="flex-1 items-center justify-center mt-4">
-        <Switch
-          trackColor={{false: '#767577', true: '#81b0ff'}}
-          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-          onValueChange={toggleSwitch}
-          value={isEnabled}
-        />
-      </SafeAreaView>
-    </SafeAreaProvider>
-    </ScrollView>
+      <ScrollView className="flex-[3] bg-[#FE6346] items-center justify-center">
+        <View className="flex-1 font-bold">
+          <Text style={{ fontSize: 128 }}>27</Text>
+          <Text
+            style={{ fontSize: 45, marginTop: -15 }}
+            className="font-medium"
+          >
+            Windy
+          </Text>
+        </View>
+        <View className="flex-1">
+          <Text style={{ fontSize: 128 }}>27</Text>
+          <Text
+            style={{ fontSize: 45, marginTop: -15 }}
+            className="font-medium"
+          >
+            Windy
+          </Text>
+        </View>
+        <View className="flex-1">
+          <Text style={{ fontSize: 128 }}>27</Text>
+          <Text
+            style={{ fontSize: 45, marginTop: -15 }}
+            className="font-medium"
+          >
+            Windy
+          </Text>
+        </View>
+        <View className="flex-1">
+          <Text style={{ fontSize: 128 }}>27</Text>
+          <Text
+            style={{ fontSize: 45, marginTop: -15 }}
+            className="font-medium"
+          >
+            Windy
+          </Text>
+        </View>
+        <View className="flex-1">
+          <Text style={{ fontSize: 128 }}>27</Text>
+          <Text
+            style={{ fontSize: 45, marginTop: -15 }}
+            className="font-medium"
+          >
+            Windy
+          </Text>
+        </View>
+        <View className="flex-1">
+          <Text style={{ fontSize: 128 }}>27</Text>
+          <Text
+            style={{ fontSize: 45, marginTop: -15 }}
+            className="font-medium"
+          >
+            Windy
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
   );
 }
