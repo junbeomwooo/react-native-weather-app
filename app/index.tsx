d0445d8ba70d4a16783b8dc5deb8ca12107aa4e9
junbeomwooo <junbeom2.woo@gmail.com>
2025-06-23 23:51:24 +0200

    Studying : ScrollView property, Expo-Location

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,41 @@
-import { ScrollView, Text, View } from "react-native";
+import { Alert, Dimensions, ScrollView, Text, View } from "react-native";
+
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
 
 export default function Index() {
+  const WINDOW_WIDTH = Dimensions.get("window").width;
+
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+
+  const permissionDenied = (title: string, msg: string) =>
+    Alert.alert(title, msg, [
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      console.log(status);
+      if (status !== "granted") {
+        permissionDenied(
+          "Permission Denied",
+          "Permission to access location was denied"
+        );
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  console.log(location);
+
   return (
     <View className="flex-1">
       <View className="flex-1 bg-orange-500 justify-center items-center">
@@ -8,57 +43,67 @@ export default function Index() {
           Copenhagen
         </Text>
       </View>
-      <ScrollView className="flex-[3] bg-[#FE6346] items-center justify-center">
-        <View className="flex-1 font-bold">
-          <Text style={{ fontSize: 128 }}>27</Text>
+      <ScrollView
+        horizontal={true}
+        pagingEnabled={true}
+        showsHorizontalScrollIndicator={false}
+        className="bg-[#FE6346] flex-1"
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View className="flex-1" style={{ width: WINDOW_WIDTH }}>
+          <Text className="text-center font-semibold" style={{ fontSize: 128 }}>
+            27
+          </Text>
           <Text
             style={{ fontSize: 45, marginTop: -15 }}
-            className="font-medium"
+            className="font-medium text-center"
           >
             Windy
           </Text>
         </View>
-        <View className="flex-1">
-          <Text style={{ fontSize: 128 }}>27</Text>
+        <View className="flex-1" style={{ width: WINDOW_WIDTH }}>
+          <Text className="text-center font-semibold" style={{ fontSize: 128 }}>
+            28
+          </Text>
           <Text
             style={{ fontSize: 45, marginTop: -15 }}
-            className="font-medium"
+            className="font-medium text-center"
           >
             Windy
           </Text>
         </View>
-        <View className="flex-1">
-          <Text style={{ fontSize: 128 }}>27</Text>
+        <View className="flex-1" style={{ width: WINDOW_WIDTH }}>
+          <Text className="text-center font-semibold" style={{ fontSize: 128 }}>
+            29
+          </Text>
           <Text
             style={{ fontSize: 45, marginTop: -15 }}
-            className="font-medium"
+            className="font-medium text-center"
           >
             Windy
           </Text>
         </View>
-        <View className="flex-1">
-          <Text style={{ fontSize: 128 }}>27</Text>
+        <View className="flex-1" style={{ width: WINDOW_WIDTH }}>
+          <Text className="text-center font-semibold" style={{ fontSize: 128 }}>
+            30
+          </Text>
           <Text
             style={{ fontSize: 45, marginTop: -15 }}
-            className="font-medium"
+            className="font-medium text-center"
           >
             Windy
           </Text>
         </View>
-        <View className="flex-1">
-          <Text style={{ fontSize: 128 }}>27</Text>
-          <Text
-            style={{ fontSize: 45, marginTop: -15 }}
-            className="font-medium"
-          >
-            Windy
+        <View className="flex-1" style={{ width: WINDOW_WIDTH }}>
+          <Text className="text-center font-semibold" style={{ fontSize: 128 }}>
+            31
           </Text>
-        </View>
-        <View className="flex-1">
-          <Text style={{ fontSize: 128 }}>27</Text>
           <Text
             style={{ fontSize: 45, marginTop: -15 }}
-            className="font-medium"
+            className="font-medium text-center"
           >
             Windy
           </Text>
